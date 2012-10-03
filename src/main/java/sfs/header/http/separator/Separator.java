c28eb2b4ad76e208c51edf94efbda6a4f956c6a5
Seiya Kawashima <skawashima@uchicago.edu>
2012-10-03 11:01:31 -0500

    Add a convenient method/ Remember to use Composite pattern here as noted by 'TODO'

--- src/main/java/sfs/header/http/separator/Separator.java
+++ src/main/java/sfs/header/http/separator/Separator.java
@@ -2,11 +2,25 @@ package sfs.header.http.separator;
 
 public class Separator {
 
+	/**
+	 * TODO Use composite pattern to take care of concatenations of other separators.
+	 */
 	private final String separator;
 	public static final Separator COLON = new Separator( ":" );
+	public static final Separator WHITE_SPACE = new Separator(" ");
+	public static final Separator SLASH = new Separator("/");
 
 	public Separator(String separator) {
 		this.separator = separator;
+	}
+
+	/**
+	 * Puts ': '. This is a convenient method to separate key and value on HTTP request header.
+	 * 
+	 * @return Colon and a whitespace like ': '.
+	 */
+	public static final String putColonAndWhiteSpace() {
+		return COLON.toString() + WHITE_SPACE.toString();
 	}
 
 	@Override
