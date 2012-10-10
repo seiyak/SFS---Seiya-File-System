57b5a633c0bb85bb3abce65aff8751228bf50261
Seiya Kawashima <skawashima@uchicago.edu>
2012-10-10 10:56:46 -0500

    Add 'ORIGIN' header entry

--- src/main/java/sfs/header/http/RequestHeaderEntry.java
+++ src/main/java/sfs/header/http/RequestHeaderEntry.java
@@ -5,6 +5,7 @@ public class RequestHeaderEntry extends HeaderEntry{
 	public static final RequestHeaderEntry ACCEPT = new RequestHeaderEntry( "Accept" );
 	public static final RequestHeaderEntry GREETING = new RequestHeaderEntry( "Greeting" );
 	public static final RequestHeaderEntry HOST = new RequestHeaderEntry( "Host" );
+	public static final RequestHeaderEntry ORIGIN = new RequestHeaderEntry( "Origin" );
 	
 	public RequestHeaderEntry(String entry) {
 		super( entry );
