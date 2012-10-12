daa4c9452f97d65050a2bb2b868a14a72b7c6cca
Seiya Kawashima <skawashima@uchicago.edu>
2012-10-12 10:45:20 -0500

    Add a method called StringUtil.getValuesOnQuery()

--- src/main/java/sfs/util/string/StringUtil.java
+++ src/main/java/sfs/util/string/StringUtil.java
@@ -77,4 +77,29 @@ public class StringUtil {
 
 		return Collections.unmodifiableMap( map );
 	}
+
+	/**
+	 * Gets values for the specified keys.
+	 * 
+	 * @param query
+	 *            Query String containing key value pairs.
+	 * @param keys
+	 *            Corresponding keys to the values that are returned.
+	 * @return String array holds the corresponding values.
+	 */
+	public static String[] getValuesOnQuery(String query, String... keys) {
+
+		if ( ( keys == null ) || ( keys.length == 0 ) ) {
+			return null;
+		}
+
+		String[] res = new String[keys.length];
+
+		Map<String, String> map = getQueryAsMap( query );
+		for ( int i = 0; i < keys.length; i++ ) {
+			res[i] = map.get( keys[i] );
+		}
+
+		return res;
+	}
 }
