759e9cc6ad90e56db1a70de5853e628ede1f83cc
Seiya Kawashima <skawashima@uchicago.edu>
2012-10-17 05:10:11 -0500

    Correct the entries

--- src/main/java/sfs/header/http/ResponseHeaderEntry.java
+++ src/main/java/sfs/header/http/ResponseHeaderEntry.java
@@ -2,9 +2,8 @@ package sfs.header.http;
 
 public class ResponseHeaderEntry extends HeaderEntry {
 
-	public static final ResponseHeaderEntry HTTP_VERSION = new ResponseHeaderEntry( "HTTP-Version" );
-	public static final ResponseHeaderEntry STATUS_CODE = new ResponseHeaderEntry( "Status-Code" );
-	public static final ResponseHeaderEntry REASON_PHRASE = new ResponseHeaderEntry( "Reason-Phrase" );
+	public static final ResponseHeaderEntry CONTENT_LENGTH = new ResponseHeaderEntry("Content-Length");
+	public static final ResponseHeaderEntry DATE = new ResponseHeaderEntry( "Date" );
 
 	public ResponseHeaderEntry(String entry) {
 		super( entry );
