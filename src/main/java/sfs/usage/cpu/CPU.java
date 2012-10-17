7554d94da7f2b5a0a9245582c4f61703f165f9df
Seiya Kawashima <skawashima@uchicago.edu>
2012-10-17 05:20:27 -0500

    Add a method called getCPUUsageAsMap() on CPU interface

--- src/main/java/sfs/usage/cpu/CPU.java
+++ src/main/java/sfs/usage/cpu/CPU.java
@@ -1,9 +1,12 @@
 package sfs.usage.cpu;
 
+import java.util.Map;
+
 import sfs.stat.cpu.CPUStat;
 
 public interface CPU {
 
 	public CPUStat[] getCPUUsage();
 	public CPUStat[] getFormattedCPUUsage();
+	public Map<String,Double> getCPUUsageAsMap();
 }
