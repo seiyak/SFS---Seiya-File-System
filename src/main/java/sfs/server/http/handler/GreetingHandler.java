7d4c3f10b85caa3adbfd93dedf583024fb28c14d
Seiya Kawashima <skawashima@uchicago.edu>
2012-10-19 12:30:30 -0500

    Add response for the greeting

--- src/main/java/sfs/server/http/handler/GreetingHandler.java
+++ src/main/java/sfs/server/http/handler/GreetingHandler.java
@@ -5,17 +5,22 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 
 import sfs.client.http.manager.NodeManager;
+import sfs.header.http.HeaderEntry;
 import sfs.header.http.RequestHeaderEntry;
+import sfs.mime.Mime;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class GreetingHandler implements HttpHandler {
+public class GreetingHandler extends AbstractHandler {
 
 	private final NodeManager nodeManager;
+	private static final String GREETING_BACK = "{\"status\": \"OK\",\"message\": \"Welcome to SFS!\"}";
+	private static final sfs.entry.Entry[] GREETING_BACK_HEADER_ENTRIES = new sfs.entry.Entry[]{new sfs.entry.Entry(HeaderEntry.CONTENT_TYPE.toString(),Mime.JSON.toString())};
 	private static Logger log = Logger.getLogger( GreetingHandler.class );
 
 	public GreetingHandler(NodeManager nodeManager) {
+		super( null );
 		this.nodeManager = nodeManager;
 	}
 
@@ -26,5 +31,8 @@ public class GreetingHandler implements HttpHandler {
 
 		nodeManager.add( exchange.getRequestHeaders().getFirst( RequestHeaderEntry.ORIGIN.toString() ), exchange
 				.getRequestHeaders().getFirst( RequestHeaderEntry.GREETING.toString() ) );
+
+		setResponseHeaders( exchange, GREETING_BACK_HEADER_ENTRIES );
+		writeResponse( exchange, GREETING_BACK.getBytes() );
 	}
 }
