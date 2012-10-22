3c97d52ae3610556847c143f15126868da2acfc3
Seiya Kawashima <skawashima@uchicago.edu>
2012-10-22 14:29:02 -0500

    Add createMessage() method

--- src/main/java/sfs/request/http/RequestMessage.java
+++ src/main/java/sfs/request/http/RequestMessage.java
@@ -1,5 +1,7 @@
 package sfs.request.http;
 
+import sfs.entry.HTTPHeaderEntry;
+import sfs.header.http.HTTPHeader;
 import sfs.header.http.ending.Ending;
 import sfs.header.http.separator.WhiteSpace;
 import sfs.request.Request;
@@ -16,4 +18,22 @@ public class RequestMessage extends Request {
 				+ HTTP_VERSION + Ending.CRLF;
 	}
 
+	/**
+	 * Creates Message with the specified verb, resource and HTTP headers.
+	 * @param verb Verb used for the resource.
+	 * @param resource Resource where the message to be sent.
+	 * @param entries Headers.
+	 * @return Message as String.
+	 */
+	public String createMessage(Verb verb, String resource, HTTPHeaderEntry[] entries) {
+
+		String str = doRequest( verb, resource );
+		HTTPHeader header = new HTTPHeader();
+		for ( HTTPHeaderEntry entry : entries ) {
+			header.put( entry.getKey(), entry.getValue() );
+		}
+
+		str += header.format();
+		return str;
+	}
 }
