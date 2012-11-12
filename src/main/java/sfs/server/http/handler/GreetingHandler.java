b2a09828d6ed7c38b393bd2422216b04eb5bdb22
Seiya Kawashima <skawashima@uchicago.edu>
2012-11-12 11:23:17 -0600

    Adjust for the change of return types of add() on NodeManager

--- src/main/java/sfs/server/http/handler/GreetingHandler.java
+++ src/main/java/sfs/server/http/handler/GreetingHandler.java
@@ -5,21 +5,28 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 
 import sfs.client.http.manager.NodeManager;
+import sfs.entry.Entry;
+import sfs.entry.HostEntry;
+import sfs.entry.StatusEntry;
 import sfs.header.http.HeaderEntry;
 import sfs.header.http.RequestHeaderEntry;
 import sfs.mime.Mime;
+import sfs.response.statuscode.StatusCode;
+import sfs.structure.Node;
+import sfs.structure.StructureNode;
+import sfs.structure.tree.MultiNode;
+import sfs.structure.tree.Star;
+import sfs.util.json.JSONUtil;
 
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
 public class GreetingHandler extends AbstractHandler {
 
-	private final NodeManager nodeManager;
-	private static final String GREETING_BACK = "{\"status\": \"OK\",\"message\": \"Welcome to SFS!\"}";
+	private final NodeManager<StructureNode> nodeManager;
 	private static final sfs.entry.Entry[] GREETING_BACK_HEADER_ENTRIES = new sfs.entry.Entry[]{new sfs.entry.Entry(HeaderEntry.CONTENT_TYPE.toString(),Mime.JSON.toString())};
 	private static Logger log = Logger.getLogger( GreetingHandler.class );
 
-	public GreetingHandler(NodeManager nodeManager) {
+	public GreetingHandler(NodeManager<StructureNode> nodeManager) {
 		super( null );
 		this.nodeManager = nodeManager;
 	}
@@ -29,10 +36,36 @@ public class GreetingHandler extends AbstractHandler {
 		log.info( "got a connection from: " + exchange.getRemoteAddress().getHostName() + " at: "
 				+ exchange.getRemoteAddress().getAddress().getHostAddress() );
 
-		nodeManager.add( exchange.getRequestHeaders().getFirst( RequestHeaderEntry.ORIGIN.toString() ), exchange
-				.getRequestHeaders().getFirst( RequestHeaderEntry.GREETING.toString() ) );
+		if ( nodeManager.isStructureNull() ) {
+			// structure is null. Use Star as default.
+			nodeManager.setStructure( new Star() );
+		}
+
+		Node node = generateNode( exchange.getRequestHeaders().getFirst( RequestHeaderEntry.GREETING.toString() ) );
+		StatusEntry status = nodeManager.add( new MultiNode( node ) );
+		String response = createResponseMessage( status.getHostEntries(), status.getStatus() );
+		writeResponseBack( exchange, status.getStatus()[0].getValue(), response );
+	}
+
+	private String createResponseMessage(HostEntry[] hostEntries, Entry[] entries) {
+
+		return JSONUtil.getJSON( hostEntries, entries );
+	}
+
+	private Node generateNode(String greeting) {
+
+		return JSONUtil.get( Node.class, greeting );
+	}
+
+	private void writeResponseBack(HttpExchange exchange, String status, String response) throws IOException {
 
 		setResponseHeaders( exchange, GREETING_BACK_HEADER_ENTRIES );
-		writeResponse( exchange, GREETING_BACK.getBytes() );
+
+		if ( status.equals( "OK" ) ) {
+			writeResponse( exchange, StatusCode._200.getNumber(), response.getBytes() );
+		}
+		else {
+			writeResponse( exchange, StatusCode._400.getNumber(), response.getBytes() );
+		}
 	}
 }
