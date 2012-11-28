8fea9b715297d174e90b6204675042c7b8fe4a4e
Seiya Kawashima <skawashima@uchicago.edu>
2012-11-28 00:50:23 -0600

    Use List instead of array to manage the greeting response header

--- src/main/java/sfs/server/http/handler/GreetingHandler.java
+++ src/main/java/sfs/server/http/handler/GreetingHandler.java
@@ -1,6 +1,8 @@
 package sfs.server.http.handler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
@@ -10,6 +12,7 @@ import sfs.entry.HostEntry;
 import sfs.entry.StatusEntry;
 import sfs.header.http.HeaderEntry;
 import sfs.header.http.RequestHeaderEntry;
+import sfs.header.http.ResponseHeaderEntry;
 import sfs.mime.Mime;
 import sfs.response.statuscode.StatusCode;
 import sfs.structure.Node;
@@ -23,12 +26,14 @@ import com.sun.net.httpserver.HttpExchange;
 public class GreetingHandler extends AbstractHandler {
 
 	private final NodeManager<StructureNode> nodeManager;
-	private static final sfs.entry.Entry[] GREETING_BACK_HEADER_ENTRIES = new sfs.entry.Entry[]{new sfs.entry.Entry(HeaderEntry.CONTENT_TYPE.toString(),Mime.JSON.toString())};
+	private final List<sfs.entry.Entry> greetingBackHeaderList;
 	private static Logger log = Logger.getLogger( GreetingHandler.class );
 
 	public GreetingHandler(NodeManager<StructureNode> nodeManager) {
 		super( null );
 		this.nodeManager = nodeManager;
+		greetingBackHeaderList = new ArrayList<sfs.entry.Entry>();
+		greetingBackHeaderList.add( new sfs.entry.Entry( HeaderEntry.CONTENT_TYPE.toString(), Mime.JSON.toString() ) );
 	}
 
 	public void handle(HttpExchange exchange) throws IOException {
@@ -60,12 +65,14 @@ public class GreetingHandler extends AbstractHandler {
 
 	private void writeResponseBack(HttpExchange exchange, String status, String response) throws IOException {
 
-		setResponseHeaders( exchange, GREETING_BACK_HEADER_ENTRIES );
-
 		if ( status.equals( "OK" ) ) {
+			greetingBackHeaderList.add(new sfs.entry.Entry(ResponseHeaderEntry.GREETING_BACK.toString(),"true"));
+			setResponseHeaders( exchange, greetingBackHeaderList.toArray( new sfs.entry.Entry[greetingBackHeaderList.size()] ));
 			writeResponse( exchange, StatusCode._200.getNumber(), response.getBytes() );
 		}
 		else {
+			greetingBackHeaderList.add(new sfs.entry.Entry(ResponseHeaderEntry.GREETING_BACK.toString(),"false"));
+			setResponseHeaders( exchange, greetingBackHeaderList.toArray( new sfs.entry.Entry[greetingBackHeaderList.size()] ));
 			writeResponse( exchange, StatusCode._400.getNumber(), response.getBytes() );
 		}
 	}
