8d1b667b1744ab963ad0bd32ea2305bacd965a03
Seiya Kawashima <skawashima@uchicago.edu>
2013-01-09 10:45:29 -0600

    Add several RequestHeaderEntry entries

--- src/main/java/sfs/header/http/RequestHeaderEntry.java
+++ src/main/java/sfs/header/http/RequestHeaderEntry.java
@@ -8,6 +8,9 @@ public class RequestHeaderEntry extends HeaderEntry{
 	public static final RequestHeaderEntry ORIGIN = new RequestHeaderEntry( "Origin" );
 	public static final RequestHeaderEntry TYPE = new RequestHeaderEntry( "Type" );
 	public static final RequestHeaderEntry LIVENESS = new RequestHeaderEntry( "Liveness" );
+	public static final RequestHeaderEntry ACCEPT_LANGUAGE = new RequestHeaderEntry( "Accept-Language" );
+	public static final RequestHeaderEntry USER_AGENT = new RequestHeaderEntry( "User-Agent" );
+	public static final RequestHeaderEntry ACCEPT_ENCODING = new RequestHeaderEntry( "Accept-Encoding" );
 
 	public RequestHeaderEntry(String entry) {
 		super( entry );
