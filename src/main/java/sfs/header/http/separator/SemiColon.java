206a9a1a0a6e8f84c32a540ae9aeb4389034d3aa
Seiya Kawashima <skawashima@uchicago.edu>
2013-01-14 23:41:00 -0600

    Add sfs.header.http.separator.SemiColon class

---
+++ src/main/java/sfs/header/http/separator/SemiColon.java
@@ -0,0 +1,12 @@
+package sfs.header.http.separator;
+
+public class SemiColon extends Separator {
+
+	public static final char SEMI_COLON = ';';
+
+	protected SemiColon() {
+		super( ";" );
+		// TODO Auto-generated constructor stub
+	}
+
+}
