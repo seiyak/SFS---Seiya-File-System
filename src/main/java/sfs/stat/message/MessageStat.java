f402e7ebb72c98362e74a5b6fcb4d3a5d1dbd7d7
Seiya Kawashima <skawashima@uchicago.edu>
2013-01-15 22:53:34 -0600

    Make sfs.stat.message.MessageStat usable for multipart/form-data type

--- src/main/java/sfs/stat/message/MessageStat.java
+++ src/main/java/sfs/stat/message/MessageStat.java
@@ -1,5 +1,14 @@
 package sfs.stat.message;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
+import sfs.header.http.HeaderEntry;
+import sfs.list.WrappedList;
+import sfs.request.http.RequestMessage;
+import sfs.util.string.StringUtil;
+
 public class MessageStat {
 
 	private int length;
@@ -11,15 +20,25 @@ public class MessageStat {
 	private int messageBodyLength;
 	private int messageBodyStartIndex = -1;
 	private boolean headerHasBeenSet;
+	private Map<HeaderEntry,String> header = null;
+	private String boundary = "";
+	private boolean fileUploadable = false;
+	private int currentContentDispositionIndex = -1;
+	private int currentContentStartIndex = -1;
+	private boolean contentDispositionSet = false;
+	private final WrappedList<ContentDisposition> contentDispositions;
+	private final static String BOUNDARY_PREFIX = "--";
 
 	public MessageStat() {
 		message = "";
+		contentDispositions = new WrappedList<ContentDisposition>( new LinkedList<ContentDisposition>() );
 	}
 
 	public MessageStat(int length, String message, boolean endOfMessage) {
 		this.setLength( length );
 		this.message = message;
 		this.endOfMessage = endOfMessage;
+		contentDispositions = new WrappedList<ContentDisposition>( new LinkedList<ContentDisposition>() );
 	}
 
 	public int getLength() {
@@ -141,6 +160,124 @@ public class MessageStat {
 		return hasBeenSet;
 	}
 
+	public void setHeader(Map<HeaderEntry, String> header) {
+		this.header = header;
+	}
+
+	public Map<HeaderEntry, String> getHeader() {
+		return header;
+	}
+
+	public String getBoundary() {
+		return boundary;
+	}
+
+	public void setBoundary(String boundary) {
+		this.boundary = BOUNDARY_PREFIX + boundary;
+	}
+
+	public boolean isFileUploadable() {
+		return fileUploadable;
+	}
+
+	/**
+	 * Checks if the header property is already set or not. If and only if not set,
+	 * the method sets the header property extracting the header part using the specified RequestMessage.
+	 * 
+	 * @param requestMessage
+	 *            Used to extract the header.
+	 */
+	public void checkAndSetHeader(RequestMessage requestMessage) {
+		if ( header == null ) {
+			setHeader( requestMessage.extractMessage( message ).getHeader() );
+		}
+	}
+
+	/**
+	 * Convenient method to check and set boundary.
+	 * 
+	 * @param index
+	 *            Where 'boundary' key word starts within the header.
+	 */
+	public void checkAndSetBoundary(int index) {
+
+		if ( index != -1 && boundary == "" ) {
+
+			setBoundary( header.get( HeaderEntry.CONTENT_TYPE ).substring( index + 1 ) );
+			fileUploadable = true;
+		}
+	}
+
+	/**
+	 * Convenient method to check and set header and boundary.
+	 * 
+	 * @param boundaryKey
+	 *            Boundary Used to separator form data and attached data.
+	 * @param requestMessage
+	 *            Used to generate header.
+	 */
+	public void checkAndSetHeaderAndBoundary(String boundaryKey, RequestMessage requestMessage) {
+		checkAndSetHeader( requestMessage );
+		checkAndSetBoundary( StringUtil
+				.searchLastIndexOfByMB( getHeader().get( HeaderEntry.CONTENT_TYPE ), boundaryKey ) );
+	}
+
+	public boolean isContentDispositionHasBeenSet() {
+		return currentContentDispositionIndex != -1 && currentContentStartIndex != -1;
+	}
+
+	public int getCurrentContentDispositionIndex() {
+		return currentContentDispositionIndex;
+	}
+
+	public boolean isContentDispositionSet() {
+		return contentDispositionSet;
+	}
+
+	public void setContentDispositionSet(boolean contentDispositionSet) {
+		this.contentDispositionSet = contentDispositionSet;
+	}
+
+	public void setCurrentContentDispositionIndex(int currentContentDispositionIndex) {
+		this.currentContentDispositionIndex = currentContentDispositionIndex;
+	}
+
+	public boolean addContentDisposition(ContentDisposition contentDisposition){
+		return contentDispositions.add( contentDisposition );
+	}
+	
+	public boolean removeContentDisposition(ContentDisposition contentDisposition){
+		return contentDispositions.remove( contentDisposition );
+	}
+	
+	public void clearContentDispositions(){
+		contentDispositions.clear();
+	}
+	
+	public Iterator<ContentDisposition> getIteratorForContentDispositions(){
+		return contentDispositions.getIterator();
+	}
+	
+	public int getSizeOfContentDispositions(){
+		return contentDispositions.getSize();
+	}
+	
+	public boolean isEmptyContentDispositions(){
+		return contentDispositions.isEmpty();
+	}
+
+	public int getCurrentContentStartIndex() {
+		return currentContentStartIndex;
+	}
+
+	public void setCurrentContentStartIndex(int currentContentStartIndex) {
+		this.currentContentStartIndex = currentContentStartIndex;
+	}
+	
+	public boolean isEndOfMessage(int index, int offset){
+		return index == message.length() - offset;
+	}
+
 	public void clearStat() {
 
 		length = 0;
@@ -152,6 +289,23 @@ public class MessageStat {
 		messageBodyContained = false;
 		messageBodyStartIndex = 0;
 		headerHasBeenSet = false;
+		clearHeader();
+		boundary = "";
+		fileUploadable = false;
+		currentContentDispositionIndex = -1;
+		contentDispositions.clear();
+		currentContentStartIndex = -1;
+	}
+
+	/**
+	 * Clears header and sets it null if and only if header peroperty is not null.
+	 */
+	private void clearHeader() {
+
+		if ( header != null ) {
+			header.clear();
+			header = null;
+		}
 	}
 
 	@Override
@@ -161,15 +315,62 @@ public class MessageStat {
 
 			MessageStat stat = (MessageStat) obj;
 			return length == stat.getLength() && message.equals( stat.getMessage() )
-					&& endOfMessage == stat.isEndOfMessage() && hasBeenSet == stat.isHasHeenSet();
+					&& endOfMessage == stat.isEndOfMessage() && hasBeenSet == stat.isHasHeenSet()
+					&& messageBodyContained == stat.isMessageBodyContained()
+					&& messageBodyType.equals( stat.getMessageBodyType() )
+					&& messageBodyLength == stat.getMessageBodyStartIndex()
+					&& messageBodyStartIndex == stat.getMessageBodyStartIndex()
+					&& headerHasBeenSet == stat.isHeaderHasBeenSet() && boundary.equals( stat.getBoundary() )
+					&& fileUploadable == stat.isFileUploadable()
+					&& currentContentDispositionIndex == stat.getCurrentContentDispositionIndex()
+					&& currentContentStartIndex == stat.getCurrentContentStartIndex()
+					&& contentDispositionSet == stat.isContentDispositionSet()
+					&& equals( stat.getIteratorForContentDispositions(), stat.getSizeOfContentDispositions() );
 		}
 
 		return false;
 	}
 
+	/**
+	 * Checks equality of contentDispositions property.
+	 * 
+	 * @param contentDispositions
+	 *            Used to be compared with contentDispositions property.
+	 * @param size
+	 *            Size of contentDispositions property.
+	 * @return True if contentDispositions property is equals to the contentDispositions property
+	 *         represented by the parameter, contentDispositions, false otherwise.
+	 */
+	private boolean equals(Iterator<ContentDisposition> contentDispositions, int size) {
+
+		if ( this.contentDispositions.getSize() == size ) {
+
+			Iterator<ContentDisposition> itr = this.contentDispositions.getIterator();
+
+			int count = 0;
+			while ( itr.hasNext() ) {
+
+				ContentDisposition contentDisposition = itr.next();
+				ContentDisposition contentDisp = contentDispositions.next();
+
+				if ( !contentDisposition.equals( contentDisp ) ) {
+					break;
+				}
+
+				count++;
+			}
+
+			if ( count == this.contentDispositions.getSize() ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
 	@Override
 	public String toString() {
 		return "{length=" + length + " message=" + message + " endOfMessage=" + endOfMessage + " hasBeenSet="
-				+ hasBeenSet + "}";
+				+ hasBeenSet + " header size=" + header.size() + "}";
 	}
 }
